7586f186b17189fabded3bc3eab7d6b2becdbfcf
Pierre <lightpower@hotmail.fr>
2021-03-17 16:29:30 +0100

    add placeholder profiles, init js script, applied css to profils elements

---
+++ listing/script.js
@@ -0,0 +1,8 @@
+// const profilPic = document.querySelectorAll(".profil-picture");
+
+// for (let i = 0; i < profilPic.length; i++)
+// i % 2 == 0 ? return : profilPic.item(i).
+
+// array.forEach(element => {
+    
+// });
