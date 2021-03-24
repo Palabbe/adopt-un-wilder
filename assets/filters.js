a90ff269da74f32f6ae4676ff7f753a96cb175b2
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-03-24 13:14:47 +0100

    added some more JS interactivity (display of sections + some event listeners) + JS Doc comments

--- assets/filters.js
+++ assets/filters.js
@@ -1,18 +1,39 @@
+const hiddenSection = [".roles-filter", ".regions-filter", "#submit-filtering"];
+
+/**
+ * Toggle the display of previously hidden section to be visible, or hidde it if forced as parameter.
+ * @param {*} selector a CSS selector
+ * @param {*} display a display value to apply to the section
+ */
+const toggleDisplaySectionBySelector = (selector, display = "block") => {
+    const section = document.querySelector(selector)
+    section.style.display = display;
+    //TODO: adding animation effect for the display
+}
+
+/**
+ * Initialize Event Listener first step of filtering
+ */
 const initFilteringAction = () => {
 
     const actionButton = document.getElementById("filtering");
 
-    actionButton.addEventListener('click', () => {
+    actionButton.addEventListener('click', (event) => {
 
+        for(let i = 0; i < hiddenSection.length; i++){
+            toggleDisplaySectionBySelector(hiddenSection[i]);
+        }
+        
+        
         const urlListing = `#roles-filter`;
-        console.log(urlListing);
         window.location.href = urlListing;
 
     });
 }
 
-
-
+/**
+ * Initialize Event Listener for skipping to all profiles action button
+ */
 const initAllProfilesAction = () => {
 
     const actionButton = document.getElementById("allProfiles");
@@ -20,16 +41,13 @@ const initAllProfilesAction = () => {
     actionButton.addEventListener('click', () => {
 
         const urlListing = `listing/listing.html`;
-        console.log(urlListing);
         window.location.href = urlListing;
 
     });
 }
 
-
-
 /**
- * Initialize roles filtering behaviour
+ * Initialize Event Listener for roles filtering behaviour
  */
 const initRolesFilter = () => {
     const rolesChoices = document.querySelectorAll(".role-choice");
@@ -54,38 +72,27 @@ const initRolesFilter = () => {
 
 }
 
-
+/**
+ * Initialize Event Listener for regions map filtering behaviour
+ */
 const initRegionsFilter = () => {
 
     const regionsMap = document.getElementById("map");
-    console.log(regionsMap);
-
     const regionsMapPaths = regionsMap.querySelectorAll(".region");
 
-    console.log(regionsMapPaths);
-
     regionsMapPaths.forEach((regionPath) => {
-
-        console.log(regionPath);
 
         regionPath.addEventListener('click', function(event) {
             event.preventDefault();
 
-
-            console.log('click region: ' + this.getAttribute("id"));
-
             if(this.classList.contains("choosen-region")){
                 this.classList.remove("choosen-region");
-                console.log('deja select region: ' + this.getAttribute("id"));
 
             } else {
                 const oldRegion = document.querySelector(".regions-map").querySelector(".choosen-region");
 
-                console.log('new select region: ' + this.getAttribute("id"));
-
                 if(oldRegion !== null){
 
-                    console.log('old select region: ' + oldRegion.getAttribute("id"));
                     oldRegion.classList.remove("choosen-region");
                 }
                 this.classList.add("choosen-region");
@@ -98,12 +105,11 @@ const initRegionsFilter = () => {
 
 }
 
-
 /**
- * Initialize checkbox icons "simulated" behaviour
+ * Initialize Event Listener for the checkbox icons "simulated" behaviour
  */
 const initInRemote = () => {
-    // Toggle check square onclick event
+    
     const inRemote = document.getElementById("inRemote");
 
     inRemote.addEventListener('click', function() {
@@ -125,74 +131,47 @@ const initInRemote = () => {
     });
 }
 
-const displayFilterSection = (section) => {
-    section.style.display = "block";
-    //TODO: adding animation effect for the display
-}
-
-const showNoFilter = () => {
-    //TODO: adding behaviour if required
-}
-
 /**
- * Initialize all the filtering interactivity flow
+ * Setup the filtering section display
  */
 const initFilteringFlow = () => {
 
-    // displaying roles filter
-    const rolesSection = document.querySelector(".roles-filter");
-    displayFilterSection(rolesSection);
+    for(let i = 0; i < hiddenSection.length; i++){
+        toggleDisplaySectionBySelector(hiddenSection[i], "none");
+    }
 
-    // hidding regions filter
-    const regionsSection = document.querySelector(".regions-filter");
-    //regionsSection.style.display = "none";
-
-    // binding region filter display
-    /*const skipAction = document.querySelector("section.roles-filter div.skip-action a");
-    
-    skipAction.addEventListener('click', (event) => {
-        event.preventDefault();
-        displayFilterSection(regionsSection);
-    });*/
-
-    // binding show all profiles
-    //showNoFilter();
 }
 
+/**
+ * Initialize Event Listener for Filtering Submission
+ */
 const initFilteringSubmit = () => {
     const filterButton = document.getElementById("filter-btn");
 
     filterButton.addEventListener('click', (event) => {
         //event.preventDefault();
 
-        //get role choice if one
         let role = null;
         const roleLiChoice = document.querySelector(".choosen-role");
         if(roleLiChoice !== null){
             role = roleLiChoice.getAttribute("id");
         }
 
-        //get region choice if one
         let region = null;
         const regionPath = document.querySelector(".choosen-region");
         if(regionPath !== null){
             region = regionPath.getAttribute("id");
         }
 
-        //check if remote
         const inRemote = document.querySelector("#inRemote").classList.contains("fa-check-square");
 
-        console.log(role);
-        console.log(inRemote);
-
         const urlListing = `listing/listing.html?role=${role}&region=${region}&remote=${inRemote}`;
-        console.log(urlListing);
         window.location.href = urlListing;
     })
 }
 
 /**
- * Initialize Landing Page filtering interactivity behaviour 
+ * Initialize Landing Page filtering main interactivity workflow behaviour 
  */
 const initLandingPage = () => {
 
