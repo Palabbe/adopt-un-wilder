5edad1e2f6c84116aea3ae705ff6c34665064b07
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-03-24 11:30:38 +0100

    Added JS filtering behaviour + navigability to sections and pages

--- assets/filters.js
+++ assets/filters.js
@@ -1,3 +1,33 @@
+const initFilteringAction = () => {
+
+    const actionButton = document.getElementById("filtering");
+
+    actionButton.addEventListener('click', () => {
+
+        const urlListing = `#roles-filter`;
+        console.log(urlListing);
+        window.location.href = urlListing;
+
+    });
+}
+
+
+
+const initAllProfilesAction = () => {
+
+    const actionButton = document.getElementById("allProfiles");
+
+    actionButton.addEventListener('click', () => {
+
+        const urlListing = `listing/listing.html`;
+        console.log(urlListing);
+        window.location.href = urlListing;
+
+    });
+}
+
+
+
 /**
  * Initialize roles filtering behaviour
  */
@@ -23,6 +53,51 @@ const initRolesFilter = () => {
     });
 
 }
+
+
+const initRegionsFilter = () => {
+
+    const regionsMap = document.getElementById("map");
+    console.log(regionsMap);
+
+    const regionsMapPaths = regionsMap.querySelectorAll(".region");
+
+    console.log(regionsMapPaths);
+
+    regionsMapPaths.forEach((regionPath) => {
+
+        console.log(regionPath);
+
+        regionPath.addEventListener('click', function(event) {
+            event.preventDefault();
+
+
+            console.log('click region: ' + this.getAttribute("id"));
+
+            if(this.classList.contains("choosen-region")){
+                this.classList.remove("choosen-region");
+                console.log('deja select region: ' + this.getAttribute("id"));
+
+            } else {
+                const oldRegion = document.querySelector(".regions-map").querySelector(".choosen-region");
+
+                console.log('new select region: ' + this.getAttribute("id"));
+
+                if(oldRegion !== null){
+
+                    console.log('old select region: ' + oldRegion.getAttribute("id"));
+                    oldRegion.classList.remove("choosen-region");
+                }
+                this.classList.add("choosen-region");
+            }
+
+
+        })
+
+    })
+
+}
+
 
 /**
  * Initialize checkbox icons "simulated" behaviour
@@ -70,27 +145,65 @@ const initFilteringFlow = () => {
 
     // hidding regions filter
     const regionsSection = document.querySelector(".regions-filter");
-    regionsSection.style.display = "none";
+    //regionsSection.style.display = "none";
 
     // binding region filter display
-    const skipAction = document.querySelector("section.roles-filter div.skip-action a");
+    /*const skipAction = document.querySelector("section.roles-filter div.skip-action a");
     
     skipAction.addEventListener('click', (event) => {
         event.preventDefault();
         displayFilterSection(regionsSection);
-    });
+    });*/
 
     // binding show all profiles
     //showNoFilter();
 }
 
+const initFilteringSubmit = () => {
+    const filterButton = document.getElementById("filter-btn");
+
+    filterButton.addEventListener('click', (event) => {
+        //event.preventDefault();
+
+        //get role choice if one
+        let role = null;
+        const roleLiChoice = document.querySelector(".choosen-role");
+        if(roleLiChoice !== null){
+            role = roleLiChoice.getAttribute("id");
+        }
+
+        //get region choice if one
+        let region = null;
+        const regionPath = document.querySelector(".choosen-region");
+        if(regionPath !== null){
+            region = regionPath.getAttribute("id");
+        }
+
+        //check if remote
+        const inRemote = document.querySelector("#inRemote").classList.contains("fa-check-square");
+
+        console.log(role);
+        console.log(inRemote);
+
+        const urlListing = `listing/listing.html?role=${role}&region=${region}&remote=${inRemote}`;
+        console.log(urlListing);
+        window.location.href = urlListing;
+    })
+}
+
 /**
- * Initialize Landing Page interactivity behaviour 
+ * Initialize Landing Page filtering interactivity behaviour 
  */
 const initLandingPage = () => {
+
+    initRegionsFilter();
+    initFilteringAction();
+    initAllProfilesAction();
     initRolesFilter();
     initFilteringFlow();
+    initFilteringSubmit();
     initInRemote();
+
 };
 
 
