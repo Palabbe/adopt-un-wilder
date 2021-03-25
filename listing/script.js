056141d009d3961ee2e57ae84add92d747077be8
ManonJosse <manonjossepro@gmail.com>
2021-03-25 16:36:15 +0100

    modal created in script.js / modal html+css udpated / main.js updated / cards.html updated / stylecards.css updated

--- listing/script.js
+++ listing/script.js
@@ -34,6 +34,9 @@ function createAllCards(student) {
     cardFooter.classList.add("cardFooter");
     card.appendChild(cardFooter);
 
+    //container boutons Linkedin + github
+    const linksContainer = document.createElement("div")
+
     //bouton linkedin
     const cardButtonsLinkedin = document.createElement('a');
     cardButtonsLinkedin.classList.add("cardButtons");
@@ -43,7 +46,6 @@ function createAllCards(student) {
     cardButtonsLinkedin.classList.add("fab", "fa-linkedin", "fa-3x");
     cardButtonsLinkedin.style.color = "var(--wcs-color-lighter)"
     cardButtonsLinkedin.appendChild(iconLinkedin);
-    cardFooter.appendChild(cardButtonsLinkedin);
 
     //bouton github
     const cardButtonsGithub = document.createElement('a');
@@ -54,26 +56,37 @@ function createAllCards(student) {
     cardButtonsGithub.classList.add("fab", "fa-github-square", "fa-3x");
     cardButtonsGithub.style.color = "var(--wcs-color-lighter)"
     cardButtonsGithub.appendChild(iconGithub);
-    cardFooter.appendChild(cardButtonsGithub);
+
+    linksContainer.appendChild(cardButtonsLinkedin)
+    linksContainer.appendChild(cardButtonsGithub)
+    cardFooter.appendChild(linksContainer)
 
     // contact me
+    const contactMeContainer = document.createElement("div")
+
     const contactMe = document.createElement("a");
     contactMe.classList.add("contactMe");
     contactMe.innerHTML = "Seduced? Contact me!"
     contactMe.href = "mailto:" + mail
-    cardFooter.appendChild(contactMe);
+
+    contactMeContainer.appendChild(contactMe)
+    cardFooter.appendChild(contactMeContainer)
 
     //see more
+    const seeMoreContainer = document.createElement("div")
+
     const seeMore = document.createElement("button");
     seeMore.classList.add("seeMore");
-    seeMore.innerHTML = "see more ";
+    seeMore.innerHTML = "see more";
     seeMore.onclick = () => openModal(id);
-    cardFooter.appendChild(seeMore);
-    
+
+    seeMoreContainer.appendChild(seeMore)
+    cardFooter.appendChild(seeMoreContainer)
+
     //chevron see more
-    const chevron = document.createElement("i");
-    chevron.classList.add("fas", "fa-chevron-down", "fa-sm");
-    seeMore.appendChild(chevron);
+    // const chevron = document.createElement("i");
+    // chevron.classList.add("fas", "fa-chevron-down", "fa-sm");
+    // seeMore.appendChild(chevron);
 }
 
 //generation cartes
@@ -89,12 +102,14 @@ for (let i = 0; i < allCards.length; i++) {
     }
 }
 
+// --------- MODAL ----------
 
 function openModal(studentId) {
     const selectedStudent = studentsInfos.find(student => student.id === studentId)
 
     const modalContainer = document.createElement("div")
     modalContainer.classList.add("modal-container")
+    modalContainer.setAttribute('id', 'modal')
 
     const modal = document.createElement("div")
     modal.classList.add("modal")
@@ -106,12 +121,84 @@ function openModal(studentId) {
 
     const profilImg = document.createElement("img")
     profilImg.classList.add("profil-img")
+    profilImg.src = selectedStudent.photo || "/assets/img/avatar.png" 
     modalHeader.appendChild(profilImg)
 
+    const close = document.createElement("div")
+    close.classList.add("close")
+    close.innerHTML = "&#10006;"
+    close.onclick = () => closeModal()
+    modalHeader.appendChild(close)
 
+    const modalBody = document.createElement("div")
+    modalBody.classList.add("modal-body")
+    modal.appendChild(modalBody)
 
-    const nameTitle = document.createElement("h2")
-    nameTitle.innerHTML = selectedStudent.name
+    const presentation = document.createElement("div")
+    presentation.classList.add("presentation")
+    modalBody.appendChild(presentation)
+
+    const Nametitle = document.createElement("h2")
+    Nametitle.classList.add("name-title")
+    Nametitle.innerHTML = selectedStudent.name
+    presentation.appendChild(Nametitle)
+
+    const speciality = document.createElement("p")
+    speciality.classList.add("subtitle")
+    speciality.innerHTML = selectedStudent.speciality
+    presentation.appendChild(speciality)
+
+    const region = document.createElement("p")
+    region.classList.add("subtitle")
+    region.innerHTML = selectedStudent.region
+    presentation.appendChild(region)
+
+    const presentationText = document.createElement("p")
+    presentationText.innerHTML = selectedStudent.presentation
+    presentation.appendChild(presentationText)
+
+    const experiences = document.createElement("div")
+    experiences.classList.add("resume-list")
+    modalBody.appendChild(experiences)
+
+    const resumeTitle = document.createElement("h3")
+    resumeTitle.classList.add("title")
+    resumeTitle.innerHTML = "Expériences"
+    experiences.appendChild(resumeTitle)
+
+    const experiencesList = document.createElement("ul")
+    selectedStudent.previousExperience.forEach(experience => {
+        const experienceItem = document.createElement("li")
+        experienceItem.innerHTML = experience
+        experiencesList.appendChild(experienceItem)
+    })
+    experiences.appendChild(experiencesList)
+
+    const softSkills = document.createElement("div")
+    softSkills.classList.add("resume-list")
+    modalBody.appendChild(softSkills)
+
+    const softSkillsTitle = document.createElement("h3")
+    softSkillsTitle.classList.add("title")
+    softSkillsTitle.innerHTML = "Soft Skills"
+    softSkills.appendChild(softSkillsTitle)
+
+    const softSkillsList = document.createElement("ul")
+    selectedStudent.softSkills.forEach(softSkills => {
+        const softSkillsItem = document.createElement("li")
+        softSkillsItem.innerHTML = softSkills
+        softSkillsList.appendChild(softSkillsItem)
+    })
+    softSkills.appendChild(softSkillsList)
+
+    const hobbies = document.createElement("div")
+    hobbies.classList.add("resume-list")
+    modalBody.appendChild(hobbies)
+
+    const hobbiesTitle = document.createElement("h3")
+    hobbiesTitle.classList.add("title")
+    hobbiesTitle.innerHTML = "Hobbies"
+    hobbies.appendChild(hobbiesTitle)
 
     const hobbyList = document.createElement("ul")
     selectedStudent.hobbies.forEach(hobby => {
@@ -119,8 +206,35 @@ function openModal(studentId) {
         hobbyItem.innerHTML = hobby
         hobbyList.appendChild(hobbyItem)
     })
+    hobbies.appendChild(hobbyList)
 
+    const links = document.createElement("div")
+    links.classList.add("links")
+    modalBody.appendChild(links)
 
+    const linkedinLink = document.createElement("a")
+    linkedinLink.href = selectedStudent.linkedinProfile
+    linkedinLink.target = "blank";
+    links.appendChild(linkedinLink)
 
-    // document.body.appendChild(modalContainer)
+    const linkedinIcon = document.createElement("img")
+    linkedinIcon.classList.add("fab", "fa-linkedin", "fa-3x");
+    linkedinIcon.style.color = "var(--wcs-color-lighter)"
+    linkedinLink.appendChild(linkedinIcon)
+
+    const githubLink = document.createElement("a")
+    githubLink.href = selectedStudent.githubProfile
+    linkedinIcon.target = "blank";
+    links.appendChild(githubLink)
+
+    const githubIcon = document.createElement("img")
+    githubIcon.classList.add("fab", "fa-linkedin", "fa-3x");
+    githubIcon.style.color = "var(--wcs-color-lighter)"
+    githubLink.appendChild(githubIcon)
+
+    document.body.appendChild(modalContainer)
+}
+
+function closeModal(){
+    document.getElementById("modal").remove()
 }
